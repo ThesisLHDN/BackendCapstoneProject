a15fc515f8369948d20da61a76c404658027c355
LamHoang-01 <nhlam011019@gmail.com>
2023-03-31 09:28:08 +0700

    Update db

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Lucas560D@tv/vn",
+  password: "",
   database: "capstone",
 });
