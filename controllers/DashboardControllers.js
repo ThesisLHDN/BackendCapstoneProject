32f257d24c8b34fd41c027da2ef37adad6eda1ad
LamHoang-01 <nhlam011019@gmail.com>
2023-05-26 19:12:12 +0700

    Burndown chart and update data

--- controllers/DashboardControllers.js
+++ controllers/DashboardControllers.js
@@ -8,14 +8,18 @@ export const getWorkload = (req, res) => {
     db.query(q, [...values], (err, data) => {
       if (err) return res.json(err);
       const today = new Date();
-      if (data[0].startDate < today && today < data[0].endDate) {
-        const q =
-          "SELECT issuestatus, COUNT(*) as numbers FROM capstone.issue WHERE projectId=? AND cycleId=? GROUP BY issuestatus;";
-        const values = [req.params.id, data[0].id];
-        db.query(q, [...values], (err, data) => {
-          if (err) return res.json(err);
-          return res.json(data);
-        });
+      if (data.length != 0) {
+        if (data[0].startDate < today && today < data[0].endDate) {
+          const q =
+            "SELECT issuestatus, COUNT(*) as numbers FROM capstone.issue WHERE projectId=? AND cycleId=? GROUP BY issuestatus;";
+          const values = [req.params.id, data[0].id];
+          db.query(q, [...values], (err, data) => {
+            if (err) return res.json(err);
+            return res.json(data);
+          });
+        } else {
+          return res.json([]);
+        }
       } else {
         return res.json([]);
       }
@@ -29,4 +33,58 @@ export const getWorkload = (req, res) => {
       return res.json(data);
     });
   }
+};
+
+export const getBurndown = (req, res) => {
+  const q =
+    "SELECT * FROM capstone.cycle WHERE id=(SELECT MAX(id) FROM cycle WHERE projectId=?)";
+  const values = [req.params.id];
+  db.query(q, [...values], (err, data) => {
+    if (err) return res.json(err);
+    const today = new Date();
+    if (data.length != 0) {
+      if (data[0].startDate < today && today < data[0].endDate) {
+        const startD = new Date(data[0].startDate);
+        startD.setDate(startD.getDate());
+        const endD = new Date(data[0].endDate);
+        endD.setDate(endD.getDate());
+
+        const numberOfDate = (endD - startD) / 86400000;
+        const dates = [];
+        for (let i = 0; i < numberOfDate; i++) {
+          const temp = new Date(data[0].startDate);
+          temp.setDate(startD.getDate() + i);
+          dates.push(
+            new Date(temp).getDate() +
+              " " +
+              new Date(temp).toLocaleString("en-us", { month: "short" })
+          );
+        }
+
+        const q =
+          "SELECT dateUpdate, pointRemain FROM sprint_log WHERE sprintId=?";
+        const values = [data[0].id];
+        db.query(q, [...values], (err, data) => {
+          if (err) return res.json(err);
+
+          if (data.length != 0) {
+            const guidelinePoint = [];
+            for (let i = numberOfDate; i > 0; i--)
+              guidelinePoint.push(
+                (data[0].pointRemain * (i - 1)) / (numberOfDate - 1)
+              );
+
+            const remainedPoint = data.map((item) => item.pointRemain);
+            return res.json([dates, guidelinePoint, remainedPoint]);
+          } else {
+            res.json([]);
+          }
+        });
+      } else {
+        return res.json([]);
+      }
+    } else {
+      return res.json([]);
+    }
+  });
 };
--- routes/DashboardRoutes.js
+++ routes/DashboardRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getWorkload } from "../controllers/DashboardControllers.js";
+import {
+  getWorkload,
+  getBurndown,
+} from "../controllers/DashboardControllers.js";
 
 const router = express.Router();
 
 router.get("/workload/:id", getWorkload);
+router.get("/burndown/:id", getBurndown);
 
 export default router;
